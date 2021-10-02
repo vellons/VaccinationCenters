932d9eef2df93d757934cb9bc2a155d5ca33a3bb
Saladone <topodidi00@gmail.com>
2021-10-02 19:03:48 +0200

    Questa roba NON funziona, per ora...

--- src/main/java/cittadini/Cittadini.java
+++ src/main/java/cittadini/Cittadini.java
@@ -1,8 +1,59 @@
 package cittadini;
 
-public class Cittadini {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!\nDEADLINE 2021-11-30");
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Cittadini{
+
+    private JPanel panelCittadini;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JPanel panelLogo2;
+    private JPanel panelLogo;
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        JFrame mainCittadini = new JFrame("Centri Vaccinali Cittadini");
+
+        mainCittadini.setContentPane(new Cittadini().panelCittadini);
+        initUI(mainCittadini);
+        System.out.println(System.getenv("CV_HOST"));
+
+        mainCittadini.pack();
+        mainCittadini.setLocationRelativeTo(null); // Mette la finestra al centro (da richiamare dopo .pack())
+        mainCittadini.setVisible(true);
+    }
+
+    public static void initUI(JFrame frame) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            System.setProperty("apple.awt.brushMetalLook", "true");
+            // use the mac system menu bar
+            System.setProperty("apple.laf.useScreenMenuBar", "true");
+            // set the "About" menu item name
+            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Centri Vaccinali Server");
+            // use smoother fonts
+            System.setProperty("apple.awt.antialiasing", "true");
+            // ref: http://developer.apple.com/releasenotes/Java/Java142RNTiger/1_NewFeatures/chapter_2_section_3.html
+            System.setProperty("apple.awt.graphics.EnableQ2DX", "true");
+            // use the system look and feel
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+    }
+
+    private void createUIComponents() throws IOException {
+        panelLogo = new JPanel();
+        BufferedImage myPicture = ImageIO.read(new File("media/PrimulaLogo.png"));
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+        panelLogo.add(picLabel);
+        panelLogo2 = new JPanel();
+        BufferedImage myPicture2 = ImageIO.read(new File("media/EatAdvisorLogo.png"));
+        JLabel picLabel2 = new JLabel(new ImageIcon(myPicture2));
+        panelLogo2.add(picLabel2);
     }
 }
