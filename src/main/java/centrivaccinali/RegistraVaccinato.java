9eda109293ff50375c9c272939f92a8505922fed
Saladone <topodidi00@gmail.com>
2021-10-11 20:26:11 +0200

    Regex semplificata e ora funzionante

--- src/main/java/centrivaccinali/RegistraVaccinato.java
+++ src/main/java/centrivaccinali/RegistraVaccinato.java
@@ -52,7 +52,7 @@ public class RegistraVaccinato{
     private int centro_id=0;
     private static List<CentroVaccinale> nomi = new ArrayList<>();
     private static List<TipologiaVaccino> tipologie = new ArrayList<>();
-    private final String CF_REGEX = "/^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i";
+    private final String CF_REGEX = "^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$";
 
     public RegistraVaccinato() throws Exception {
 
