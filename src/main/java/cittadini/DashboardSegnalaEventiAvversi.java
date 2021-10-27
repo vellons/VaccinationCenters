fee57e548e5c129fbf64a6be945367e917521df6
manuelmacaj <49785644+manuelmacaj@users.noreply.github.com>
2021-10-27 13:40:57 +0200

    add a check if a note has got a space bar character in the beginning.

--- src/main/java/cittadini/DashboardSegnalaEventiAvversi.java
+++ src/main/java/cittadini/DashboardSegnalaEventiAvversi.java
@@ -20,7 +20,7 @@ public class DashboardSegnalaEventiAvversi {
     private JPanel panelLogo;
     private JLabel lbInfo;
     protected static List<EventoAvversoPerLista> listEvento = new ArrayList<>();
-    private final List<EventoAvversoPerLista> listFinalEvento = new ArrayList<>();
+    private final List<EventoAvversoPerLista> listApproveEvento = new ArrayList<>();
     private final List<EventoAvversoPerLista> listErrorEvento = new ArrayList<>();
     private final List<EventoAvversoPerLista> listNessunEvento = new ArrayList<>();
 
@@ -37,18 +37,19 @@ public class DashboardSegnalaEventiAvversi {
                 if (listNessunEvento.size() == listEvento.size()) { // se l'utente non ha segnalato niente
                     jOptionPanelMessageDialog("Non hai segnalato nessun evento. Se non vuoi segnalare,\nchiudi la finestra.", "Nessun evento avverso segnalato");
                 } else { // se l'utente ha segnalato qualcosa
-                    //   if (!(listErrorEvento.size() > 0)) { // se non ci sono elementi (considerati errori), inoltro al server gli eventi avversi
-                    sendToServer();
-                    jOptionPanelMessageDialog("Segnalazione inviato con successo", "Invio riuscito");
-                    Cittadini.closePreviousWindow(DashboardEventiAvversiElenco.segnalaEventiAvversiFrame);
-                    chekListEventoCauseIsStatic();
-                    try {
-                        Cittadini.reloadDashboardEventiAvversiElenco(Login.elencoEventiAvversi);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
+                    if (!(listErrorEvento.size() > 0)) { // se non ci sono elementi (considerati errori), inoltro al server gli eventi avversi
+                        sendToServer();
+                        jOptionPanelMessageDialog("Segnalazione inviato con successo", "Invio riuscito");
+                        Cittadini.closePreviousWindow(DashboardEventiAvversiElenco.segnalaEventiAvversiFrame);
+                        chekListEventoCauseIsStatic();
+                        try {
+                            Cittadini.reloadDashboardEventiAvversiElenco(Login.elencoEventiAvversi);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } else {
+                        jOptionPanelMessageDialog("Attenzione: le note non devono contenere spazi iniziali. Correggere e riprovare", "Invio fallito");
                     }
-                    //   } else
-                    //     jOptionPanelMessageDialog("", "Invio fallito");
                 }
             }
         });
@@ -63,19 +64,27 @@ public class DashboardSegnalaEventiAvversi {
         clearLists();
 
         for (EventoAvversoPerLista elem : listEvento) {
-            if (elem.getValoreSeverita() > 0) {
-                // if (elem.getNota().isEmpty())
-                //     listErrorEvento.add(elem);
-                //  else
-                listFinalEvento.add(elem);
+            if (elem.getValoreSeverita() > 0) { // se l'utente ha spostato lo slider di un evento avverso, verifico la nota
+                if (!elem.getNota().isEmpty()) { // se la nota contiene qualcosa...
+                    boolean res = firstLetter(elem.getNota()); //... verifico la stringa
+                    if (res) // se composto da spazi...
+                        listErrorEvento.add(elem); //... inserisco nella lista dedicata agli eventi avversi non approvati
+                    else // altrimenti...
+                        listApproveEvento.add(elem); // ...aggiungo nella lista degli eventi avversi approvati
+                } else // se l'utente non è interessato ad aggiungere una nota a riguardo di un particolare evento avverso//...
+                    listApproveEvento.add(elem); //  ...aggiungo nella lista degli eventi avversi approvati
             } else
                 listNessunEvento.add(elem);
         }
     }
 
+    private boolean firstLetter(String input) {
+        return Character.isSpaceChar(input.charAt(0));
+    }
+
     private void clearLists() {
-        if (!listFinalEvento.isEmpty())
-            listFinalEvento.clear();
+        if (!listApproveEvento.isEmpty())
+            listApproveEvento.clear();
         if (!listErrorEvento.isEmpty())
             listErrorEvento.clear();
         if (!listNessunEvento.isEmpty())
@@ -84,7 +93,7 @@ public class DashboardSegnalaEventiAvversi {
 
     private void sendToServer() {
         DatabaseCVInterface db = ServerConnectionSingleton.getDatabaseInstance();
-        for (EventoAvversoPerLista elem : listFinalEvento) {
+        for (EventoAvversoPerLista elem : listApproveEvento) {
             try {
                 EventoAvverso ea = new EventoAvverso(Login.utenteLoggato.getId(), elem.getTipologiaEvento().getId(), elem.getValoreSeverita(), elem.getNota().trim());
                 db.inserisciNuovoEventoAvversoCittadino(ea);
