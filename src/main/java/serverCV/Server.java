0c1319561e7f68b6cb3dfb76977a9aefe0d309ac
vellons <vellons@users.noreply.github.com>
2021-11-02 00:34:34 +0100

    Server --no-gui and JavaDoc

--- src/main/java/serverCV/Server.java
+++ src/main/java/serverCV/Server.java
@@ -17,6 +17,18 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * La classe Server permette la connessione al database.
+ * Tramite RMI è possibile invocare metodi per eseguire operazioni sul database.
+ * Le operazioni sul database avvengono mediante metodi definiti nella classe DatabaseCV.
+ * <br>
+ * Questo programma può essere eseguito con i seguenti parametri:<br>
+ * --remote: imposta l'hostname RMI all'indirizzo IP della WAN (internet) a cui è collegato il PC.<br>
+ * --no-gui: avvia il programma senza interfaccia grafica, solo output su console.<br>
+ *
+ * @author Alex Vellone e Macaj Manuel
+ * @see DatabaseCV
+ */
 public class Server {
     private JTextField tfUsernameDB;
     private JPasswordField tfPasswordDB;
@@ -30,14 +42,18 @@ public class Server {
     private final static String REGISTRY_NAME = "CVDatabaseServer";
     private final static int REGISTRY_PORT = 1099;
     private static boolean ALLOW_REMOTE = false;
-    private String WAN_IP = null;
-    private String LAN_IP = null;
+    private static boolean NO_GUI = false;
     private String username;
     private String password;
     private String host;
     private Connection conn;
     DatabaseCV dbCV;
 
+    /**
+     * Costruttore della classe Server
+     *
+     * @throws RemoteException eccezione per RMI
+     */
     public Server() throws RemoteException {
 
         btnDisconnetti.setEnabled(false);
@@ -61,21 +77,23 @@ public class Server {
             connectToDB();
         });
 
-        btnDisconnetti.addActionListener(e -> {
-            disconnectToDB();
-        });
+        btnDisconnetti.addActionListener(e -> disconnectToDB());
 
         // Print the server IP
+        String WAN_IP = null;
+        String LAN_IP;
         try {
             Socket socket = new Socket();
             socket.connect(new InetSocketAddress("1.1.1.1", 80));
-            URL url = new URL("http://checkip.amazonaws.com/");
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            WAN_IP = br.readLine();
             LAN_IP = socket.getLocalAddress().toString().replace("/", "");
-            logMessage("Server LAN IP: " + LAN_IP);
-            logMessage("Server WAN IP: " + WAN_IP);
             socket.close();
+            logMessage("Server LAN IP: " + LAN_IP);
+            if (ALLOW_REMOTE) {
+                URL url = new URL("http://checkip.amazonaws.com/");
+                BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+                WAN_IP = br.readLine();
+                logMessage("Server WAN IP: " + WAN_IP);
+            }
         } catch (Exception ignored) {
         }
 
@@ -109,27 +127,58 @@ public class Server {
         }
     }
 
-    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        JFrame mainServer = new JFrame("Centri Vaccinali - Server");
+    /**
+     * Main.
+     * Punto di partenza del programma Server.
+     *
+     * @param args argomenti in ingresso
+     * @throws UnsupportedLookAndFeelException eccezione
+     * @throws ClassNotFoundException eccezione
+     * @throws InstantiationException eccezione
+     * @throws IllegalAccessException eccezione
+     * @throws RemoteException eccezione
+     */
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException, RemoteException {
         for (String option : args) {
             if (option.toLowerCase().contains("--remote")) {
                 ALLOW_REMOTE = true;
                 System.err.println("ALLOW_REMOTE = " + ALLOW_REMOTE);
             }
+            if (option.toLowerCase().contains("--no-gui")) {
+                NO_GUI = true;
+                System.err.println("NO_GUI = " + NO_GUI);
+            }
         }
 
-        try {
-            mainServer.setContentPane(new Server().panelServer);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        initUI(mainServer);
+        if (NO_GUI) {
+            new Server();
+        } else {
+            JFrame mainServer = new JFrame("Centri Vaccinali - Server");
+            try {
+                mainServer.setContentPane(new Server().panelServer);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+            initUI(mainServer);
 
-        mainServer.pack();
-        mainServer.setLocationRelativeTo(null); // Mette la finestra al centro (da richiamare dopo .pack())
-        mainServer.setVisible(true);
+            mainServer.pack();
+            mainServer.setLocationRelativeTo(null); // Mette la finestra al centro (da richiamare dopo .pack())
+            mainServer.setVisible(true);
+        }
     }
 
+    /**
+     * <code>initUI</code> &egrave; una procedura per inizializzare l'interfaccia
+     * utente su una finestra e per finalizzarne le impostazioni.
+     *
+     * @param frame &egrave; il frame sul quale applicare le impostazioni
+     *              &egrave; dichiarato <strong>void</strong> in quanto non restituisce alcun valore
+     *              &egrave; dichiarata <strong>static</strong> così da non doverla istanziare creando un oggetto
+     * @throws ClassNotFoundException se non trova la classe da caricare
+     * @throws UnsupportedLookAndFeelException e le classi look and feel richieste non sono presenti sul sistema
+     * @throws InstantiationException se per qualche motivo la classe non può essere istanziata
+     * @throws IllegalAccessException quando si cerca di effettuare l'accesso ad un campo laddove non &egrave; possibile
+     */
     public static void initUI(JFrame frame) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Definisce il comportamento della finestra
         ImageIcon imageIcon = new ImageIcon("media/IconServer.png");
@@ -151,6 +200,12 @@ public class Server {
         }
     }
 
+    /**
+     * Procedura per stampare un messaggio in console e sulla GUI.
+     * Questo metodo aggiunge automaticamente la data e l'ora corrente.
+     *
+     * @param message messaggio da stampare
+     */
     public void logMessage(String message) {
         String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH:mm:ss"));
         String out = "[" + currentTime + "] " + message + "\n";
@@ -159,6 +214,9 @@ public class Server {
         textAreaServerStatus.setCaretPosition(textAreaServerStatus.getDocument().getLength()); // autoscroll automatico
     }
 
+    /**
+     * Metodo da eseguire per la connessione al database
+     */
     private void connectToDB() {
         try {
             Class.forName("org.postgresql.Driver");
@@ -182,6 +240,9 @@ public class Server {
         }
     }
 
+    /**
+     * Metodo da eseguire per disconnettersi dal database
+     */
     private void disconnectToDB() {
         try {
             if (conn != null) {
